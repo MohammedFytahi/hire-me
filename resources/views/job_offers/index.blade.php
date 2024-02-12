1a9ea512d72e2f096c13b1a051a562713e1f4d7a
MohammedFytahi <mohammedfytahi567@gmail.com>
2024-02-12 17:31:58 +0100

    hireme

--- resources/views/job_offers/index.blade.php
+++ resources/views/job_offers/index.blade.php
@@ -1,48 +1,60 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
-    <script src="https://cdn.tailwindcss.com"></script>
-</head>
-<body class="bg-gray-100">
+    <!DOCTYPE html>
+    <html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <meta http-equiv="X-UA-Compatible" content="ie=edge">
+        <title>Hirme</title>
+        <script src="https://cdn.tailwindcss.com"></script>
+    </head>
+    <body class="bg-gray-100">
+
+        <form action="{{ route('jobs.search') }}" method="GET">
+            <input type="text" name="keyword" placeholder="Recherchez des offres d'emploi">
+            <button type="submit">Rechercher</button>
+        </form>
+        @foreach ($jobs as $job)
+        <div class="container mx-auto px-4 my-24 md:px-6">
+        <!-- Section: Design Block -->
+        <section class="max-w-3xl mx-auto mb-32 bg-white rounded-lg shadow-lg dark:shadow-black/20 p-6">
+        <img src="https://mdbcdn.b-cdn.net/img/new/slides/198.jpg"
+            class="mb-6 w-full rounded-lg shadow-lg dark:shadow-black/20" alt="image" />
     
-<div class="container mx-auto px-4 my-24 md:px-6">
-    <!-- Section: Design Block -->
-    <section class="max-w-3xl mx-auto mb-32 bg-white rounded-lg shadow-lg dark:shadow-black/20 p-6">
-      <img src="https://mdbcdn.b-cdn.net/img/new/slides/198.jpg"
-        class="mb-6 w-full rounded-lg shadow-lg dark:shadow-black/20" alt="image" />
-  
-      <div class="mb-6 flex items-center">
-        <img src="https://mdbcdn.b-cdn.net/img/Photos/Avatars/img (23).jpg" class="mr-2 h-8 rounded-full" alt="avatar"
-          loading="lazy" />
-        <div>
-          <span> Published <u>15.07.2020</u> by </span>
-          <a href="#!" class="font-medium">Anna Maria Doe</a>
+        <div class="mb-6 flex items-center">
+            <img src="{{ asset($job->user->profile_image) }}" class="mr-2 h-8 rounded-full" alt="avatar"
+            loading="lazy" />
+            <div>
+            <span> Published <u>{{$job->created_at}}<</u> by </span>
+            <a href="#!" class="font-medium">{{ $job->user?->name }}</a>
+            </div>
         </div>
-      </div>
-  
-      <h1 class="mb-6 text-3xl font-bold">
-        An intriguing title for an interesting article
-      </h1>
-  
-      <p>
-        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Eligendi
-        harum tempore cupiditate asperiores provident, itaque, quo ex iusto
-        rerum voluptatum delectus corporis quisquam maxime a ipsam nisi
-        sapiente qui optio! Dignissimos harum quod culpa officiis suscipit
-        soluta labore! Expedita quas, nesciunt similique autem, sunt,
-        doloribus pariatur maxime qui sint id enim. Placeat, maxime labore.
-        Dolores ex provident ipsa impedit, omnis magni earum. Sed fuga ex
-        ducimus consequatur corporis, architecto nesciunt vitae ipsum
-        consequuntur perspiciatis nulla esse voluptatem quos dolorum delectus
-        similique eum vero in est velit quasi pariatur blanditiis incidunt
-        quam.
-      </p>
-    </section>
-    <!-- Section: Design Block -->
-  </div>
-</body>
-</html>
+    
+        <h1 class="mb-6 text-3xl font-bold">
+            {{$job->title}}
+        </h1>
+    
+        <p class="break-words">
+            {{$job->description}}
+        </p>
+        <div class="flex">
+            @if(auth()->check() && $job->user_id === auth()->id())
+        <form action="{{ route('jobs.destroy', $job->id) }}" method="POST">
+            @csrf
+            @method('DELETE')
+            <button type="submit">Supprimer</button>
+        </form>
+
+        <form action="" method="POST">
+            @csrf
+            <button type="submit" class="bg-blue-500 text-white px-4 py-2 rounded">Apply</button>
+        </form>
+        </div>
+        
+        </section>
+        <!-- Section: Design Block -->
+    </div>  
+        @endforeach
+        
+
+    </body>
+    </html>
--- routes/web.php
+++ routes/web.php
@@ -1,78 +1,85 @@
-<?php
+    <?php
 
-use App\Http\Controllers\ProfileController;
-use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\HomeController;
-use App\Http\Controllers\Auth\RegisteredUserController;
-use App\Http\Controllers\UtilisateurController;
-use App\Http\Controllers\CompetenceController;
-use App\Http\Controllers\ExperienceController;
-use App\Http\Controllers\Formcontroller;
-use App\Http\Controllers\LanguageController;
-use App\Http\Controllers\CompanyController;
-use App\Http\Controllers\PDFController;
-use App\Http\Controllers\JobOfferController;
-;
+    use App\Http\Controllers\ProfileController;
+    use Illuminate\Support\Facades\Route;
+    use App\Http\Controllers\HomeController;
+    use App\Http\Controllers\Auth\RegisteredUserController;
+    use App\Http\Controllers\UtilisateurController;
+    use App\Http\Controllers\CompetenceController;
+    use App\Http\Controllers\ExperienceController;
+    use App\Http\Controllers\Formcontroller;
+    use App\Http\Controllers\LanguageController;
+    use App\Http\Controllers\CompanyController;
+    use App\Http\Controllers\PDFController;
+    use App\Http\Controllers\JobOfferController;
+    ;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "web" middleware group. Make something great!
-|
-*/
+    /*
+    |--------------------------------------------------------------------------
+    | Web Routes
+    |--------------------------------------------------------------------------
+    |
+    | Here is where you can register web routes for your application. These
+    | routes are loaded by the RouteServiceProvider and all of them will
+    | be assigned to the "web" middleware group. Make something great!
+    |
+    */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+    Route::get('/', function () {
+        return view('welcome');
+    });
 
-Route::get('/dashboard', function () {
-    return view('dashboard');
-})->middleware(['auth', 'verified'])->name('dashboard');
-Route::get('/home', [HomeController::class, 'index']);
+    Route::get('/dashboard', function () {
+        return view('dashboard');
+    })->middleware(['auth', 'verified'])->name('dashboard');
+    Route::get('/home', [HomeController::class, 'index']);
 
-Route::middleware('auth')->group(function () {
-    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
-    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
-    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
-    Route::get('/section', [UtilisateurController::class, 'showform'])->name('section.form');
-    Route::get('/company', [CompanyController::class, 'showform'])->name('company.form');
+    Route::middleware('auth')->group(function () {
+        Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+        Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+        Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+        Route::get('/section', [UtilisateurController::class, 'showform'])->name('section.form');
+        Route::get('/company', [CompanyController::class, 'showform'])->name('company.form');
 
-    Route::get('/cvform', [Formcontroller::class, 'index'])->name('formCv');
-    Route::post('/store', [Formcontroller::class, 'store'])->name('cv.store');
-    Route::get('/cv/cursus', [Formcontroller::class, 'getUserCursus'])->name('getUserCursus');
-    Route::delete('/cursus/{id}', [Formcontroller::class, 'destroy'])->name('cursus.destroy');
+        Route::get('/cvform', [Formcontroller::class, 'index'])->name('formCv');
+        Route::post('/store', [Formcontroller::class, 'store'])->name('cv.store');
+        Route::get('/cv/cursus', [Formcontroller::class, 'getUserCursus'])->name('getUserCursus');
+        Route::delete('/cursus/{id}', [Formcontroller::class, 'destroy'])->name('cursus.destroy');
 
-    
-    Route::get('/user/experiences', [ExperienceController::class, 'getUserExperience'])->name('getUserExperience');
-    Route::post('/experience', [ExperienceController::class, 'store'])->name('experience.store');
-    Route::delete('/experience/{id}', [ExperienceController::class, 'destroy'])->name('experience.destroy');
+        
+        Route::get('/user/experiences', [ExperienceController::class, 'getUserExperience'])->name('getUserExperience');
+        Route::post('/experience', [ExperienceController::class, 'store'])->name('experience.store');
+        Route::delete('/experience/{id}', [ExperienceController::class, 'destroy'])->name('experience.destroy');
 
-    
+        
 
-Route::post('/language', [LanguageController::class, 'storeLanguage'])->name('language.store');
-Route::get('/user/language', [LanguageController::class, 'getUserLanguage'])->name('getUserLanguage');
-Route::delete('/language/{id}', [LanguageController::class, 'deleteLanguage'])->name('language.destroy');
+    Route::post('/language', [LanguageController::class, 'storeLanguage'])->name('language.store');
+    Route::get('/user/language', [LanguageController::class, 'getUserLanguage'])->name('getUserLanguage');
+    Route::delete('/language/{id}', [LanguageController::class, 'deleteLanguage'])->name('language.destroy');
 
-Route::post('/competence', [CompetenceController::class, 'store'])->name('competence.store');
-Route::get('/user/competence', [CompetenceController::class, 'getUserCompetence'])->name('getUserCompetence');
-Route::delete('/competence/{id}', [CompetenceController::class, 'destroy'])->name('competence.destroy');
+    Route::post('/competence', [CompetenceController::class, 'store'])->name('competence.store');
+    Route::get('/user/competence', [CompetenceController::class, 'getUserCompetence'])->name('getUserCompetence');
+    Route::delete('/competence/{id}', [CompetenceController::class, 'destroy'])->name('competence.destroy');
 
 
-Route::get('/generate-pdf', [PDFController::class, 'generatePDF'])->name('generate.pdf');
-Route::get('/cv', [PDFController::class, 'index'])->name('cv');
+    Route::get('/generate-pdf', [PDFController::class, 'generatePDF'])->name('generate.pdf');
+    Route::get('/cv', [PDFController::class, 'index'])->name('cv');
 
-    Route::post('/section', [UtilisateurController::class, 'store'])->name('section');
+        Route::post('/section', [UtilisateurController::class, 'store'])->name('section');
 
-    Route::post('/company', [CompanyController::class, 'store'])->name('company');
+        Route::post('/company', [CompanyController::class, 'store'])->name('company');
 
-    Route::get('/offers', [JobOfferController::class, 'create'])->name('job_offers.create');
+        Route::get('/job-offers/create', [JobOfferController::class, 'create'])->name('job_offers.create');
 
-    Route::post('/offers', [JobOfferController::class, 'store'])->name('job_offers.store');
 
-});
+        Route::post('/offers', [JobOfferController::class, 'store'])->name('job_offers.store');
 
-require __DIR__.'/auth.php';
+        Route::get('/jobs/search', [JobOfferController::class, 'search'])->name('jobs.search');
+        // routes/web.php
+
+Route::delete('/jobs/{job}', [JobOfferController::class, 'destroy'])->name('jobs.destroy');
+
+
+    });
+
+    require __DIR__.'/auth.php';
